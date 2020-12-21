cdef6aa1c30b9dc4faf90d320806eb6a711f9f33
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-12-21 17:24:26 -0300

    add newTodoItem page

--- app/src/main/java/com/devdossantos/sofie/view/MainFragment.kt
+++ app/src/main/java/com/devdossantos/sofie/view/MainFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.devdossantos.sofie.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainFragment : Fragment() {
@@ -20,5 +22,13 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+        val addButton = view.findViewById<FloatingActionButton>(R.id.btn_addNewTodo_main)
+
+        addButton.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_newTodoFragment)
+        }
+
     }
 }
